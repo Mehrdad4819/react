b2dd74f566e497589fe12c64e64c4a430e39cac6
Mehrdad Nouri <mnourib12@gmail.com>
2023-06-10 14:08:25 +0330

    init

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'John Doe',
+      username: 'johndoe',
+      email: 'johndoe@example.com'
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      username: 'janesmith',
+      email: 'janesmith@example.com'
+    },
+    {
+      id: 3,
+      name: 'Alice Johnson',
+      username: 'alicej',
+      email: 'alicej@example.com'
+    },
+    {
+      id: 4,
+      name: 'Bob Anderson',
+      username: 'boband',
+      email: 'boband@example.com'
+    },
+    {
+      id: 5,
+      name: 'Sarah Williams',
+      username: 'sarahw',
+      email: 'sarahw@example.com'
+    },
+    {
+      id: 6,
+      name: 'Michael Brown',
+      username: 'michaelb',
+      email: 'michaelb@example.com'
+    },
+    {
+      id: 7,
+      name: 'Emily Davis',
+      username: 'emilyd',
+      email: 'emilyd@example.com'
+    },
+    {
+      id: 8,
+      name: 'David Taylor',
+      username: 'davidt',
+      email: 'davidt@example.com'
+    },
+    {
+      id: 9,
+      name: 'Olivia Martinez',
+      username: 'oliviam',
+      email: 'oliviam@example.com'
+    },
+    {
+      id: 10,
+      name: 'James Wilson',
+      username: 'jamesw',
+      email: 'jamesw@example.com'
+    }
+  ];
+  
